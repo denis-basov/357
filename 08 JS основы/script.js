63988cae22c4b26bc13e8a67909199a2e05995d7
Denis Basov <den4er@mail.ru>
2023-09-25 19:02:10 +0300

    js основы

---
+++ 08 JS основы/script.js
@@ -0,0 +1,113 @@
+// let age = 22; // объвляю переменную и кладу в нее значение
+// let login = "admin";
+
+// let age = 23;
+// let firstName = "Chimpoesh";
+// let myName = "Alexander";
+// console.log(age);
+// console.log(firstName);
+// console.log(myName);
+
+// let boys = 15;
+// let girls = 17;
+// console.log(boys + girls);
+
+// let a = 10;
+// let b = 5;
+// console.log(a * b);
+
+// let login = "rabbit";
+// let age = 2;
+// let gender = "male";
+// console.log(login);
+// console.log(age);
+// console.log(gender);
+
+// let name = "Ira";
+// let surname = "Ivanova";
+// let job = "doctor";
+// console.log(name);
+// console.log(surname);
+// console.log(job);
+
+// let name = "anton";
+// let passworld = "admin";
+
+// console.log(name);
+// console.log(passworld);
+// console.log(5 + 5);
+
+// let cats = 6;
+// let dogs = 9;
+// let animals = cats + dogs;
+
+// console.log(animals);
+// console.log(animals);
+
+// let fruits = 77;
+// let newFruits = fruits - 1; // 76
+// console.log(newFruits);
+
+// "-", "+" - приоритет 12
+// "=" - приоритет 2
+// let fruits = 77;
+// fruits = fruits - 1;
+// fruits = fruits + 10;
+// console.log(fruits);
+
+// const pi = 3.14;
+// pi = pi + 1;
+
+// let firstName = "Ivan";
+// let lastName = "Ivanov";
+// console.log(firstName, lastName);
+
+// let postIndex = "456784";
+// let phoneNumber = "34858737";
+// let hobby = "Читать";
+
+// let firstName = "Павел";
+// let lastName = "Петров";
+// let hobby = "Строить";
+
+// Меня зовут Павел, моя фамилия Петров.
+// let greet = "<h1>Меня зовут " + firstName + ", моя фамилия " + lastName + "</h1>";
+// console.log(greet);
+// document.write(greet);
+
+// let greet = `<h1>Меня зовут ${firstName}, моя фамилия ${lastName}. Моё любимое занятие - ${hobby}</h1>`;
+// console.log(greet);
+// document.write(greet);
+
+// let carModel = "2101";
+// let carMaker = "Ваз";
+// let power = 76;
+// // сформируйте строку:
+// // Автомобиль Ваз 2101 мощность 76 л.с
+// let greet = `<h1 class="head">Автомобиль ${carMaker} ${carModel} мощность ${power} л.с.</h1>`;
+// console.log(greet);
+// document.write(greet);
+
+// let carModel = "Chaser";
+// let carMaker = "Toyota";
+// let motor = 2.5;
+
+// // let opinion = `Автомобиль ${carMaker} ${carModel}, объем двигателя ${motor + 1} литра`;
+// let opinion = `Автомобиль ${carMaker + " " + carModel}, объем двигателя ${motor + 1} литра`;
+// console.log(opinion);
+
+// let item = "Помидоры";
+// let price = 2.6;
+// let qty = 6;
+
+// let str = `Мы купили ${qty}кг ${item} общая сумма за покупку
+//  $${price * qty}`;
+// console.log(str);
+
+// let login = "Ivan";
+// let isAdmin = false; // true false
+
+// let cats;
+// console.log(cats);
+// cats = 5;
+// console.log(cats);

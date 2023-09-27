a7f8fac9b52debc01a9fdc327bbf302b575e8468
Denis Basov <den4er@mail.ru>
2023-09-27 19:02:25 +0300

    js массивы и объекты

---
+++ 11 JS Циклы/arrays.js
@@ -0,0 +1,54 @@
+let users = ["Иван", "Алексей", "Светлана", "Петр", "Алиса", "Неизвестный"];
+let numbers = [1, 4, 6, 3, 2, 8, 4, 3];
+let mixed = [true, 1, null, false, "hello", NaN, [1, 2, 3]];
+let fruits = ["Киви", "Ананас", "Кокос", "Апельсин", "Банан", "Яблоко"];
+let pets = ["cat", "dog", "bat", "mouse", "pig", "goat", "sheep", "cow", "chicken"];
+let animals = ["ant", "bison", "camel", "duck", "elephant", "cat", "dog"];
+
+let phones = [
+  [345, 678, 137, 789, 568, 236],
+  [542, 678, 345, 867],
+  [872, 589, 126, 875, 345],
+];
+
+let user = {
+  firstName: "Иван",
+  lastName: "Иванов",
+  age: 22,
+  hobby: "читать",
+  isAdmin: false,
+  city: "Москва",
+  avatar: "image.jpg",
+  children: true,
+};
+
+let user1 = {
+  firstName: "Иван",
+  lastName: "Иванов",
+  age: 22,
+  hobbies: ["читать", "готовить", "гулять", "спать"],
+  images: ["image.jpg", "rabbit.jpg"],
+  address: {
+    city: "Москва",
+    street: "Пушкина",
+    house: 33,
+  },
+};
+
+let devices = [
+  {
+    device: "Телевизор",
+    type: "Ламповый",
+    colour: "Цветной",
+  },
+  {
+    device: "Монитор",
+    type: "ЖК",
+    colour: "Цветной",
+  },
+  {
+    device: "Часы",
+    type: "Механические",
+    colour: "Серебристый",
+  },
+];

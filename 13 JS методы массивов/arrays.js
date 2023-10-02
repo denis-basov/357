fce7caa77355b188b63786b7d3263bc57cf1fadf
Denis Basov <den4er@mail.ru>
2023-10-02 19:02:04 +0300

    функции

---
+++ 13 JS методы массивов/arrays.js
@@ -0,0 +1,12 @@
+let users = ["Иван", "Алексей", "Светлана", "Петр", "Алиса", "Неизвестный"];
+let numbers = [1, 4, 6, 3, 2, 8, 4, 3];
+let mixed = [true, 1, null, false, "hello", NaN, [1, 2, 3]];
+let fruits = ["Киви", "Ананас", "Кокос", "Апельсин", "Банан", "Яблоко"];
+let pets = ["cat", "dog", "bat", "mouse", "pig", "goat", "sheep", "cow", "chicken"];
+let animals = ["ant", "bison", "camel", "duck", "elephant", "cat", "dog"];
+
+let phones = [
+  [345, 678, 137, 789, 568, 236],
+  [542, 678, 345, 867],
+  [872, 589, 126, 875, 345],
+];
---
+++ 13 JS методы массивов/script.js
@@ -0,0 +1,12 @@
+// push - добавление в конец массива
+// let newArrLen = fruits.push("Абрикос", "Виноград");
+// console.log(fruits);
+// console.log(newArrLen);
+
+// pop - удаляет последний элемент из массива
+// let lastEl1 = animals.pop();
+// let lastEl2 = animals.pop();
+
+// console.log(animals);
+// console.log(lastEl1);
+// console.log(lastEl2);

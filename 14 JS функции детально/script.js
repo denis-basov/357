83c61c4b93eb061ff89349c91457705d38a8266d
Denis Basov <den4er@mail.ru>
2023-10-04 15:14:54 +0300

    методы обратного вызова

--- 14 JS функции детально/script.js
+++ 14 JS функции детально/script.js
@@ -119,3 +119,99 @@
 
 // let myGreet = greet; // передаем функцию greet как значение
 // myGreet();
+
+//1
+/*
+let sayHello = function () {
+  console.log("Hello");
+};
+
+let print = function (myFunc) {
+  myFunc();
+  myFunc();
+};
+
+print(sayHello); // передаем функцию sayHello как значение
+*/
+
+// 1.1
+/*
+let print = function (myFunc) {
+  myFunc();
+  myFunc();
+};
+
+print(function () {
+  console.log("Hello");
+}); // передаем функцию sayHello как значение
+*/
+
+//2
+/*
+let myLogin = "Ivan";
+let sayHello = function (login) {
+  console.log(`Hello, ${login}`);
+};
+
+let print = function (myFunc, login) {
+  myFunc(login);
+};
+
+print(sayHello, myLogin);
+*/
+
+//3
+/*
+let myLogin = "Ivan";
+let sayHello = function (login) {
+  console.log(`Hello, ${login}`);
+};
+
+let sayBye = function (login) {
+  console.log(`Bye, ${login}`);
+};
+
+let print = function (myFunc, login) {
+  myFunc(login);
+};
+
+print(sayHello, myLogin);
+print(sayBye, myLogin);
+*/
+
+//4
+/*
+let head1 = document.querySelector("h1"); // получаем заголовок h1
+let handler = function () {
+  console.log("Hello");
+};
+// при клике на заголовок вывести строку в консоль
+head1.addEventListener("click", handler);
+*/
+
+//4.1
+/*
+let head1 = document.querySelector("h1"); // получаем заголовок h1
+
+// при клике на заголовок вывести строку в консоль
+head1.addEventListener("click", function () {
+  console.log("Hello");
+});
+*/
+
+// возврат функции из другой функции
+/*
+function getCurrentYear(currentYear) {
+  function calcAge(birthYear) {
+    let age = currentYear - birthYear;
+    console.log(age);
+  }
+
+  return calcAge;
+}
+
+let getUserAge = getCurrentYear(2023);
+getUserAge(2002);
+getUserAge(1952);
+getUserAge(1985);
+*/
---
+++ 15 JS Методы обратного вызова/arrays.js
@@ -0,0 +1,171 @@
+// let users = ["Иван", "Алексей", "Светлана", "Петр", "Алиса", "Неизвестный"];
+let numbers = [1, 4, 6, 3, 2, 8, 4, 3];
+let mixed = [true, 1, null, false, "hello", NaN, [1, 2, 3]];
+let fruits = ["Киви", "Ананас", "Кокос", "Апельсин", "Банан", "Яблоко"];
+let pets = ["cat", "dog", "bat", "mouse", "pig", "goat", "sheep", "cow", "chicken"];
+let animals = ["ant", "bison", "camel", "duck", "elephant", "cat", "dog"];
+let votes = [
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+  "y",
+  "n",
+  "n",
+  "y",
+  "y",
+];
+
+let phones = [
+  [345, 678, 137, 789, 568, 236],
+  [542, 678, 345, 867],
+  [872, 589, 126, 875, 345],
+];
+
+let user = {
+  firstName: "Иван",
+  lastName: "Иванов",
+  age: 22,
+  hobby: "читать",
+  isAdmin: false,
+  city: "Москва",
+  avatar: "image.jpg",
+  children: true,
+};
+
+let user1 = {
+  firstName: "Иван",
+  lastName: "Иванов",
+  age: 22,
+  hobbies: ["читать", "готовить", "гулять", "спать"],
+  images: ["image.jpg", "rabbit.jpg"],
+  address: {
+    city: "Москва",
+    street: "Пушкина",
+    house: 33,
+  },
+};
+
+let devices = [
+  {
+    device: "Телевизор",
+    type: "Ламповый",
+    colour: "Цветной",
+  },
+  {
+    device: "Монитор",
+    type: "ЖК",
+    colour: "Цветной",
+  },
+  {
+    device: "Часы",
+    type: "Механические",
+    colour: "Серебристый",
+  },
+];
+
+let users = [
+  {
+    fName: "Иван",
+    lName: "Иванов",
+    hobbies: ["Спать", "Гулять", "Читать"],
+  },
+  {
+    fName: "Анна",
+    lName: "Иванова",
+    hobbies: ["Спать", "Читать"],
+  },
+  {
+    fName: "Ирина",
+    lName: "Сидорова",
+    hobbies: ["Гулять", "Читать"],
+  },
+];
+
+let cats = [
+  {
+    name: "Ллойд",
+    color: "Черный",
+    age: 12,
+    avatar: "img/1.jpg",
+    owner: {
+      name: "Василий",
+      city: "Москва",
+      phones: [333, 543, 123, 87876],
+    },
+  },
+  {
+    name: "Барсик",
+    color: "Серый",
+    age: 6,
+    avatar: "img/2.jpg",
+    owner: {
+      name: "Анна",
+      city: "Москва",
+      phones: [523, 653, 532],
+    },
+  },
+  {
+    name: "Мурка",
+    color: "Белый",
+    age: 2,
+    avatar: "img/3.jpg",
+    owner: {
+      name: "Инна",
+      city: "Самара",
+      phones: [187, 466, 643],
+    },
+  },
+  {
+    name: "Пусик",
+    color: "Серый",
+    age: 5,
+    avatar: "img/4.jpg",
+    owner: {
+      name: "Николай",
+      city: "Москва",
+      phones: [345, 678, 543],
+    },
+  },
+];

27fb73a8d600daad428b679e1ae69b21a9af653e
Denis Basov <den4er@mail.ru>
2023-10-18 14:14:48 +0300

    заготовка

---
+++ 26 PHP Передача данных в ссылке/data.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'id' => 1, 'maker' => 'Ferrari', 'model' => 'F50', 'made_year' => '1995', 'top_speed' => 325, 'acceleration_to_100' => 4,
+        'power' => 517, 'weight' => 1350, 'image' => 'img/ferrarif50.jpg'
+    ],
+    [
+        'id' => 2, 'maker' => 'Lamborghini', 'model' => 'Countach', 'made_year' => '1995', 'top_speed' => 325, 'acceleration_to_100' => 4,
+        'power' => 517, 'weight' => 1350, 'image' => 'img/lamborghinicountach.jpg'
+    ],
+    [
+        'id' => 3, 'maker' => 'Koenigsegg', 'model' => 'Agera R', 'made_year' => '1995', 'top_speed' => 325, 'acceleration_to_100' => 4,
+        'power' => 517, 'weight' => 1350, 'image' => 'img/koenigsegg.jpg'
+    ],
+    [
+        'id' => 4, 'maker' => 'Bugatti', 'model' => 'Chiron', 'made_year' => '2016', 'top_speed' => 420, 'acceleration_to_100' => 3,
+        'power' => 1000, 'weight' => 1888, 'image' => 'img/bugatti.jpg'
+    ],
+];
